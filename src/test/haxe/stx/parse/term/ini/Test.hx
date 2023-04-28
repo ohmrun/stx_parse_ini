70344b2e82e24dcfeedef9dc623a666103a4cbce
0b1kn00b <polysemantic@gmail.com>
2023-04-28 17:53:22 +0200

    all tests running

--- src/test/haxe/stx/parse/term/ini/Test.hx
+++ src/test/haxe/stx/parse/term/ini/Test.hx
@@ -43,47 +43,64 @@ class IniTest extends TestCase{
       ]
     }
   ];
+  public function eq_op(){
+    return Eq.Cluster(new stx.assert.parse.ini.eq.Data());
+  }
   public function test(){
     final val = __.resource("test").string();
     final v   = Ini.parse(val).fudge();
-    eq(res,v,Eq.Cluster(new stx.assert.parse.ini.eq.Data()));
+    eq(res,v,eq_op());
   }
-  // public function test_line(){
-  //   final val = "a = b\n";
-  //   final v   = Ini.line().and_(Parsers.Eof()).apply(val.reader()).toUpshot();
-  //   //trace(v);
-  // }
-  // public function test_line_lhs(){
-  //   final val = "asdn=";
-  //   final v   = Ini.line_lhs().apply(val.reader()).toUpshot();
-  //   trace(v);
-  //   // for(x in v.fudge()){
-  //   //   trace(x);
-  //   // }
-  // }
-  // public function test_section_head(){
-  //   final val = " [ ok]  \n";
-  //   final v   = Ini.section_head().and_(Parsers.Eof()).apply(val.reader()).toUpshot();
-  //   trace(v);
-  // }
-  // public function test_line_rhs(){
-  //   final val = "asdasd\n";
-  //   final v   = Ini.line_rhs().apply(val.reader()).toUpshot();
-  //   trace(v);
-  // }
-  // public function test_line(){
-  //   final val = "bab = asdasd\n";
-  //   final v   = Ini.line().apply(val.reader()).toUpshot();
-  //   trace(v);
-  // }
+  public function test_line(){
+    final val = "a = b\n";
+    final v   = Ini.line().and_(Parsers.Eof()).apply(val.reader()).toUpshot().fudge().flatten();
+    for(x in v){
+      same(tuple2("a","b"),x.tup());
+    }
+  }
+  public function test_line_lhs(){
+    final val = "asdn=";
+    final v   = Ini.line_lhs().apply(val.reader()).toUpshot();
+    for(x in v.fudge()){
+      same("asdn",x);
+    }
+  }
+  public function test_section_head(){
+    final val = " [ ok]  \n";
+    final v   = Ini.section_head().and_(Parsers.Eof()).apply(val.reader()).toUpshot();
+    for(x in v.fudge()){
+      same(x,"ok");
+    }
+  }
+  public function test_line_rhs(){
+    final val = "asdasd\n";
+    final v   = Ini.line_rhs().apply(val.reader()).toUpshot();
+    for(x in v.fudge()){
+      same(x,'asdasd');
+    }
+  }
+  public function test_lineI(){
+    final val = "bab = asdasd\n";
+    final v   = Ini.line().apply(val.reader()).toUpshot();
+    for(x in v.fudge()){
+      for(y in x){
+        same(y.tup(),tuple2("bab","asdasd"));
+      }
+    }
+  }
   public function test_lines(){
     final val = "bab = asdasd\n ascv = booy\n\n \nbbb=8\n";
     final v   = Ini.lines().and_(Parsers.Eof()).apply(val.reader()).toUpshot();
-    trace(v);
+    for(x in v.fudge()){
+      final a = ["bbb" => "8","bab" => "asdasd","ascv" => "booy"];
+      eq(x,a,Eq.StringMap(Eq.String()));
+    }
   }
-  // public function test_empty_line(){
-  //   final val = " \n";
-  //   final v   = Ini.line().apply(val.reader()).toUpshot();
-  //   trace(v);
-  // }
+  public function test_empty_line(){
+    final val = " \n";
+    final v   = Ini.line().apply(val.reader()).toUpshot();
+    for(x in v.fudge()){
+      same(None,x);
+    }
+  }
 }
